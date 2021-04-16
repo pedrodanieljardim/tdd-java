f7ef6702e7a3a2fa7894f77d9ddd4e814acb24f5
pedrodanieljardim <pedro.d.jardim@hotmail.com>
2021-04-15 22:40:59 -0300

    classe main e entidade calculadora

---
+++ src/com/br/pedro/entities/Calculadora.java
@@ -0,0 +1,8 @@
+package com.br.pedro.entities;
+
+public class Calculadora {
+    // Atalho para gerar atalho no intelliJ = Shift + Crtl + T
+    public static int sum(int a, int b){
+        return a+b;
+    }
+}
